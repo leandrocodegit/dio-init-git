3763fa008b29c4b14994a6675c0399d506c6965b
leandrooliveira <leandro.appcode@gmail.com>
2022-01-22 18:00:50 -0300

    Iniciao com git

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.printf("Hello word");
+
+        for (int i =0;  i < 100; i++){
+
+            try {
+                Thread.sleep(1000);
+                System.out.println(i);
+            }
+            catch (Exception err){
+
+            }
+
+        }
+
+    }
+
 }
